7a828ac2c692af4dcb54a5ea5faf8a4c070bb04e
aldipee <aldipeee@gmail.com>
2020-05-05 12:05:27 +0700

    change server

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,6 @@
 const config = {
-  DATA_URL: `http://localhost:5001/`,
-  DATA_FILE: `http://localhost:5001/public/users/`
+  DATA_URL: `http://159.89.196.9:5001/`,
+  DATA_FILE: `http://159.89.196.9:5001/public/users/`
 }
 
 export default config
